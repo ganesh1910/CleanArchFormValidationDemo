83516538743750dcbbcbb2ed537ddca65eb17510
Ganesh Khandagale <68702026+ganesh1910@users.noreply.github.com>
2022-08-09 17:21:47 +0530

    experimental commit

--- app/src/main/java/com/gktech/cleanarchformvalidationdemo/presentation/MainActivity.kt
+++ app/src/main/java/com/gktech/cleanarchformvalidationdemo/presentation/MainActivity.kt
@@ -25,7 +25,6 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         print("Experimental branch 2")
-        print("temp brach")
         setContent {
             CleanArchFormValidationDemoTheme {
                 // A surface container using the 'background' color from the theme
