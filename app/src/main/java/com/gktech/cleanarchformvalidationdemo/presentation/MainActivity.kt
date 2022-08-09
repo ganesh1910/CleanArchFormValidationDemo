8c1121b0b79aae7f8dedec332665e0ec0b0bce45
Ganesh Khandagale <68702026+ganesh1910@users.noreply.github.com>
2022-08-09 16:03:01 +0530

    wrong commit

--- app/src/main/java/com/gktech/cleanarchformvalidationdemo/presentation/MainActivity.kt
+++ app/src/main/java/com/gktech/cleanarchformvalidationdemo/presentation/MainActivity.kt
@@ -25,7 +25,7 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val p ;
+        val pkkcxkc ;
 
         setContent {
             CleanArchFormValidationDemoTheme {
