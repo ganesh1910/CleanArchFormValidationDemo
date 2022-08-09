2dafa06db187967664ccb41dbe959702cde4f3db
Ganesh Khandagale <68702026+ganesh1910@users.noreply.github.com>
2022-08-09 16:10:13 +0530

    wel commit

--- app/src/main/java/com/gktech/cleanarchformvalidationdemo/presentation/MainActivity.kt
+++ app/src/main/java/com/gktech/cleanarchformvalidationdemo/presentation/MainActivity.kt
@@ -25,8 +25,6 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val p ;
-
         setContent {
             CleanArchFormValidationDemoTheme {
                 // A surface container using the 'background' color from the theme
